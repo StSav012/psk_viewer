e2c20248f956079bdf912101aabea42c6b7ef949
StSav012 <stsav012@gmail.com>
2021-05-16 13:20:38 +0300

    Check for changes before loading data from the repo

--- updater.py
+++ updater.py
@@ -6,14 +6,36 @@ import json
 import urllib.request
 import zipfile
 from pathlib import Path
-from typing import Any, BinaryIO, Dict, Final, List, Optional, TextIO, Union
+from typing import BinaryIO, Dict, Final, List, TextIO
 
 
 def update(user: str, repo_name: str, branch: str = 'master') -> None:
     extraction_root: Path = Path.cwd()
+
+    commits_url: Final[str] = f'https://api.github.com/repos/{user}/{repo_name}/commits?page=1&per_page=1'
+    r: http.client.HTTPResponse = urllib.request.urlopen(commits_url, timeout=1)
+    if r.getcode() != 200:
+        return
+    content: bytes = r.read()
+    if not content:
+        return
+    d: List[Dict[str, Dict[str, Dict[str, str]]]] = json.loads(content)
+    if not isinstance(d, list) or not len(d):
+        return
+    date: Final[str] = d[0].get('commit', dict()).get('author', dict()).get('date', '')
+    if (extraction_root / Path('version.py')).exists():
+        try:
+            import version
+
+            if version.UPDATED == f'{date}':
+                return
+        except (OSError, ImportError, ModuleNotFoundError):
+            pass
+    text_f_out: TextIO
+    with (extraction_root / Path('version.py')).open('wt') as text_f_out:
+        text_f_out.write(f'UPDATED: str = "{date}"\n')
+
     repo_url: Final[str] = f'https://github.com/{user}/{repo_name}/archive/{branch}.zip'
-    r: http.client.HTTPResponse
-    content: bytes
     r = urllib.request.urlopen(repo_url, timeout=1)
     if r.getcode() != 200:
         return
@@ -27,21 +49,3 @@ def update(user: str, repo_name: str, branch: str = 'master') -> None:
             binary_f_out: BinaryIO
             with (extraction_root / Path(member.filename).relative_to(root)).open('wb') as binary_f_out:
                 binary_f_out.write(content)
-    commits_url: Final[str] = f'https://api.github.com/repos/{user}/{repo_name}/commits?page=1&per_page=1'
-    r = urllib.request.urlopen(commits_url, timeout=1)
-    if r.getcode() != 200:
-        return
-    content = r.read()
-    if not content:
-        return
-    d: List[Dict[str,
-                 Dict[str,
-                      Dict[str,
-                           Optional[Union[str, bool]]]]]] \
-        = json.loads(content)
-    if not isinstance(d, list) or not len(d):
-        return
-    date: Final[Optional[Union[Any, str, bool]]] = d[0].get('commit', dict()).get('author', dict()).get('date', '')
-    text_f_out: TextIO
-    with (extraction_root / Path('version.py')).open('wt') as text_f_out:
-        text_f_out.write(f'UPDATED: str = "{date}"\n')
