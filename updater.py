394babb78e18a04e8184853d11c6869495f9467e
stsav012 <stsav012@gmail.com>
2023-01-30 17:25:50 +0300

    Minor refactoring

--- updater.py
+++ updater.py
@@ -6,8 +6,11 @@ import io
 import json
 import urllib.request
 import zipfile
+from contextlib import suppress
 from pathlib import Path
-from typing import BinaryIO, Final, TextIO
+from typing import Final
+
+__all__ = ['update']
 
 
 def update(user: str, repo_name: str, branch: str = 'master') -> None:
@@ -21,32 +24,28 @@ def update(user: str, repo_name: str, branch: str = 'master') -> None:
     if not content:
         return
     d: list[dict[str, dict[str, dict[str, str]]]] = json.loads(content)
-    if not isinstance(d, list) or not len(d):
+    if not isinstance(d, list) or not d:
         return
     date: Final[str] = d[0].get('commit', dict()).get('author', dict()).get('date', '')
     if (extraction_root / Path('version.py')).exists():
-        try:
+        with suppress(BaseException):  # suppress everything
             import version
 
             if version.UPDATED == f'{date}':
                 return
-        except (OSError, ImportError, ModuleNotFoundError):
-            pass
-    text_f_out: TextIO
-    with (extraction_root / Path('version.py')).open('wt') as text_f_out:
-        text_f_out.write(f'UPDATED: str = "{date}"\n')
+    (extraction_root / Path('version.py')).write_text(f'UPDATED: str = "{date}"\n')
 
     repo_url: Final[str] = f'https://github.com/{user}/{repo_name}/archive/{branch}.zip'
     r = urllib.request.urlopen(repo_url, timeout=1)
     if r.getcode() != 200:
         return
+    inner_zip: zipfile.ZipFile
     with zipfile.ZipFile(io.BytesIO(r.read())) as inner_zip:
         root: Path = Path(f'{repo_name}-{branch}/')
+        member: zipfile.ZipInfo
         for member in inner_zip.infolist():
             if member.is_dir():
                 continue
             content = inner_zip.read(member)
             (extraction_root / Path(member.filename).relative_to(root)).parent.mkdir(parents=True, exist_ok=True)
-            binary_f_out: BinaryIO
-            with (extraction_root / Path(member.filename).relative_to(root)).open('wb') as binary_f_out:
-                binary_f_out.write(content)
+            (extraction_root / Path(member.filename).relative_to(root)).write_bytes(content)
